28aaca87b5c7fc30e4c57154de8166eaffe5c3ba
etrigo <bjorn.sjostedt@etrigo.com>
2019-07-10 13:03:50 +0200

    copy and modal works

--- script.js
+++ script.js
@@ -18,5 +18,33 @@ function copyFunction() {
   var copyText = document.getElementById("siteUrl");
   copyText.select();
   document.execCommand("copy");
-  alert("Se där ja, " + copyText.value + ", är kopierad. Dela var du vill :)");
+  /*alert("Se där ja, " + copyText.value + ", är kopierad. Dela var du vill :)");*/
+}
+
+window.onload = function () {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var urlField = document.getElementById("siteUrl");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  urlField.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
